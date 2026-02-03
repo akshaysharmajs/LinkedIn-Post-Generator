63b0796204fdb12207849249f8ecb4a89f74398f
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2026-02-02 17:41:00 -0800

    Generating Tags on Posts

---
+++ preprocess.py
@@ -0,0 +1,44 @@
+import json
+from llm_helper import llm
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+
+def extract_metadata(post: str):
+    template = '''
+    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language and tags. 
+    3. tags is an array of text tags. Extract maximum two tags.
+    4. Language should be English or Hinglish (Hinglish means hindi + english)
+    
+    Here is the actual post on which you need to perform this task:  
+    {post}
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={'post': post})
+    try:
+        json_parser = JsonOutputParser()
+        result = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+
+    return result
+
+def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
+    enriched_posts = []
+    with open(raw_file_path, encoding="utf-8") as file:
+        posts = json.load(file)
+        for post in posts:
+            post = posts[-1]
+            metadata = extract_metadata(post['text'])
+            post_with_metadata = post | metadata
+            enriched_posts.append(post_with_metadata)
+
+    for epost in enriched_posts:
+        print(epost)
+
+if __name__ == "__main__":
+    process_posts("data/raw_posts.json")
